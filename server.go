acd310ce6d32a49e2f786ded613474cbc19be828
Nasaruddin <nasaruddin@mekari.com>
2022-03-04 18:31:34 +0700

    crash course 02 | middleware

--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/pace-noge/golang-gin-poc/controllers"
+	"github.com/pace-noge/golang-gin-poc/middlewares"
 	"github.com/pace-noge/golang-gin-poc/services"
+	ginDump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -11,8 +13,25 @@ var (
 	videoController controllers.VideoController = controllers.New(videoService)
 )
 
+// func setupLogOutput() {
+// 	// create gin.log file
+// 	f, _ := os.Create("gin.log")
+
+// 	// write log to file and stdout
+// 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+// }
+
 func main() {
-	server := gin.Default()
+	// setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(
+		gin.Recovery(),
+		middlewares.BasicAuth(),
+		middlewares.Logger(),
+		ginDump.Dump(),
+	)
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
--- services/video-service.go
+++ services/video-service.go
@@ -12,7 +12,9 @@ type videoService struct {
 }
 
 func New() VideoService {
-	return &videoService{}
+	return &videoService{
+		videos: []entities.Video{},
+	}
 }
 
 func (service *videoService) Save(video entities.Video) entities.Video {
